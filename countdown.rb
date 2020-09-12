496776e8a1a893474acf8863fb594836a500e0bf
Aiden Marques <aiden.marques94@gmail.com>
2020-09-12 18:31:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,12 +3,12 @@
 def countdown (count)
   while count > 0 
     puts "#{count} SECOND(S)!"
-    countdown_while_sleep
+    sleep(5)
     count -= 1
   end
     "HAPPY NEW YEAR!"
 end
   
-  def countdown_while_sleep(countdown)
-    sleep(countdown)
-  end
+ # def countdown_while_sleep(countdown)
+  #  sleep(countdown)
+  # end
